6f1295ee981ee49b5bced51edd6c7f8a6c74b276
Antoś Jurevič <yurevich-anton@mail.ru>
2019-04-02 18:07:44 +0300

    Added actions on create and open buttons, some fixs in UI

--- Editor/src/Controller/MainController.java
+++ Editor/src/Controller/MainController.java
@@ -5,15 +5,32 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class MainController {
+    
+    @FXML
+    private Button addQuestionButton;
+
+    @FXML
+    private Button deleteQuestionButton;
+
+    @FXML
+    private Button changeQuestionButton;
 
     @FXML
     private Button openPackageButton;
 
     @FXML
     private Button savePackageButton;
+
+    @FXML
+    private TableView<?> questionsTable;
 
     @FXML
     private Button createPackageButton;
@@ -23,6 +40,9 @@ public class MainController {
 
     @FXML
     private TextField packageNameField;
+
+    private ObservableList<?> table = FXCollections.observableArrayList("1 - физ-ра", "2 - лекции Титовой", "3 - лабы по ЦОСиИ", "4 - курсач по СиФО", "5 - зачёт Глеца");
+
 
     @FXML
     void initialize() {
@@ -42,4 +62,30 @@ public class MainController {
     public void setDifficultyChoiceBox(ObservableList<String> list) {
         difficultyChoiceBox.setItems (list);
     }
+
+    public void openPackageButtonMethod() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Открыть файл пакета");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Файлы пакетов", ".kwq"));
+        File file = fileChooser.showOpenDialog(new Stage());
+        setAllEnable();
+    }
+
+    public void setAllEnable() {
+        packageNameField.setDisable(false);
+        difficultyChoiceBox.setDisable(false);
+        questionsTable.setDisable(false);
+    }
+
+    public void createPackageButtonMethod() {
+        setAllEnable();
+    }
+
+    public void savePackageButtonMethod() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Сохранить пакет");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Файлы пакетов", ".kwq"));
+        File file = fileChooser.showSaveDialog(new Stage());
+    }
+
 }
