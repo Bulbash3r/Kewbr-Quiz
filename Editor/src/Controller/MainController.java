71fb5b1c41a7b08fe3363e15ff10f8b19f866505
Antoś Jurevič <yurevich-anton@mail.ru>
2019-04-02 16:50:31 +0300

    Added UI

---
+++ Editor/src/Controller/MainController.java
@@ -0,0 +1,21 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MainController {
+
+    @FXML
+    private Button openPackageButton;
+
+    @FXML
+    private Label packageNameLabel;
+
+    @FXML
+    private Button savePackageButton;
+
+    @FXML
+    private Button createPackageButton;
+
+}
