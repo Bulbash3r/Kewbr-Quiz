1a778afe60a19fe663564df19fdb1b0963e88df8
NikitaTer <nikitatet100@gmail.com>
2019-04-09 20:19:12 +0300

    Editor refactoring and integration in main app (done)

--- Application/Kewbr Quiz/src/App/Models/Pack.java
+++ Application/Kewbr Quiz/src/App/Models/Pack.java
@@ -1,16 +1,21 @@
 package App.Models;
 
+import com.google.gson.*;
+
 import java.io.File;
 import java.io.FilenameFilter;
+import java.lang.reflect.Array;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 public class Pack {
 
     private String name;
     private int difficulty;
-    private ArrayList<String> questions;
-    private ArrayList<String> answers;
+    private ArrayList<Question> questions;
     private Date date;
 
     public Pack(String name, int difficulty, Date date) {
@@ -18,22 +23,18 @@ public class Pack {
         this.difficulty = difficulty;
         this.date = date;
         questions = new ArrayList<>();
-        answers = new ArrayList<>();
     }
 
     public void addNewQuestion(String question, String answer) {
-        questions.add(question);
-        answers.add(answer);
+        questions.add(new Question(question, answer));
     }
 
     public void deleteQuestion(int index) {
         questions.remove(index - 1);
-        answers.remove(index - 1);
     }
 
     public void editQuestion(int index, String question, String answer) {
-        questions.set(index - 1, question);
-        answers.set(index - 1, answer);
+        questions.set(index - 1, new Question(question, answer));
     }
 
     public String getName() {
@@ -60,16 +61,12 @@ public class Pack {
         this.date = date;
     }
 
-    public ArrayList<ArrayList<String>> getQuestions() {
-        ArrayList<ArrayList<String>> lists = new ArrayList<ArrayList<String>>(2);
-        lists.add(questions);
-        lists.add(answers);
-        return lists;
+    public ArrayList<Question> getQuestions() {
+        return questions;
     }
 
-    public void setQuestions(ArrayList<String> questions, ArrayList<String> answers) {
-        this.questions = questions;
-        this.answers = answers;
+    public void setQuestions(List<Question> questions) {
+        this.questions = new ArrayList<>(questions);
     }
 
     public static class Filter implements FilenameFilter {
